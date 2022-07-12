c7bcefa13ae6ee58b85f75a0938ecec1cbdd8f07
Sedwna <109065711+sedwna@users.noreply.github.com>
2022-07-12 15:59:33 +0430

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#define SIZE 10
-void chart()
-{
-    // start
-    printf("\033[96m%c\033[0m", 43);
-    for (size_t i = 0; i < SIZE * 4 -1; i++)
-        printf("\033[96m%c\033[0m", 45);
-    printf("\033[96m%c\033[0m \n", 43); // end start
-    for (size_t j = 0; j < 8; j++)
-    {
-
-        // start mid
-        for (size_t i = 0; i < SIZE - 1; i++)
-        {
-            printf("%c", 179);
-            printf("    ");
-        }
-        printf("\n"); // end mid
-
-        // finish
-        printf("%c", 43);
-        for (size_t i = 0; i < SIZE *4 -1; i++)
-            printf("%c", 45);
-        printf("%c\n", 43); // end finish
-    }
-}
-
-           
+         
 int main()
 {
     char ch;
@@ -36,7 +9,7 @@ int main()
     unsigned int column = 8;
     int array[row][column];
     chart();
-    /*while (ch != 'E')
+    while (ch != 'E')
     {
         for (size_t i = 0; i < row; i++)
     {
@@ -59,7 +32,7 @@ int main()
 
 
 
-    }*/
+    }
 
     return 0;
 }
