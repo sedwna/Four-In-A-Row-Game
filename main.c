020d6f2241fb150f943cf3a965a53d85b0bf1446
Sedwna <109065711+sedwna@users.noreply.github.com>
2022-07-13 13:17:38 +0430

    Update main.c

--- main.c
+++ main.c
@@ -4,35 +4,7 @@
          
 int main()
 {
-    char ch;
-    unsigned int row = 8;
-    unsigned int column = 8;
-    int array[row][column];
-    chart();
-    while (ch != 'E')
-    {
-        for (size_t i = 0; i < row; i++)
-    {
-        for (size_t j = 0; j < column; j++)
-        {
-            array[row][column] = 0;
-        }
-    }
-
-    for (size_t i = 0; i < row; i++)
-    {
-        for (size_t j = 0; j < column; j++)
-        {
-            printf("%d ", array[row][column]);
-        }
-        printf("\n");
-    }
-    ch = getch();
-    system("cls");
-
-
-
-    }
+    
 
     return 0;
 }
